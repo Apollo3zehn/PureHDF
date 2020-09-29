8d88a7e0c076167322830a2d6e0e6581dca3ebd8
Vincent Wilms <vwilms@outlook.com>
2020-09-29 15:16:53 +0200

    Fix failing tests.

--- src/HDF5.NET/Utils/ArrayExtensions.cs
+++ src/HDF5.NET/Utils/ArrayExtensions.cs
@@ -11,8 +11,9 @@ namespace HDF5.NET
     {
         public static unsafe T[,] ToArray2D<T>(this T[] data, long dim0, long dim1) where T : unmanaged
         {
-            ArrayExtensions.ValidateInputData(data, new long[] { dim0, dim1 });
-            var output = new T[dim0, dim1];
+            var dims = new long[] { dim0, dim1 };
+            ArrayExtensions.ValidateInputData(data, dims);
+            var output = new T[dims[0], dims[1]];
 
             fixed (void* ptr = output)
             {
@@ -24,8 +25,9 @@ namespace HDF5.NET
 
         public static unsafe T[,,] ToArray3D<T>(this T[] data, long dim0, long dim1, long dim2) where T : unmanaged
         {
-            ArrayExtensions.ValidateInputData(data, new long[] { dim0, dim1 , dim2 });
-            var output = new T[dim0, dim1, dim2];
+            var dims = new long[] { dim0, dim1, dim2 };
+            ArrayExtensions.ValidateInputData(data, dims);
+            var output = new T[dims[0], dims[1], dims[2]];
 
             fixed (void* ptr = output)
             {
@@ -37,8 +39,9 @@ namespace HDF5.NET
 
         public static unsafe T[,,,] ToArray4D<T>(this T[] data, long dim0, long dim1, long dim2, long dim3) where T : unmanaged
         {
-            ArrayExtensions.ValidateInputData(data, new long[] { dim0, dim1, dim2, dim3 });
-            var output = new T[dim0, dim1, dim2, dim3];
+            var dims = new long[] { dim0, dim1, dim2, dim3 };
+            ArrayExtensions.ValidateInputData(data, dims);
+            var output = new T[dims[0], dims[1], dims[2], dims[3]];
 
             fixed (void* ptr = output)
             {
@@ -50,8 +53,9 @@ namespace HDF5.NET
 
         public static unsafe T[,,,,] ToArray5D<T>(this T[] data, long dim0, long dim1, long dim2, long dim3, long dim4) where T : unmanaged
         {
-            ArrayExtensions.ValidateInputData(data, new long[] { dim0, dim1, dim2, dim3, dim4 });
-            var output = new T[dim0, dim1, dim2, dim3, dim4];
+            var dims = new long[] { dim0, dim1, dim2, dim3, dim4 };
+            ArrayExtensions.ValidateInputData(data, dims);
+            var output = new T[dims[0], dims[1], dims[2], dims[3], dims[4]];
 
             fixed (void* ptr = output)
             {
@@ -63,8 +67,9 @@ namespace HDF5.NET
 
         public static unsafe T[,,,,,] ToArray6D<T>(this T[] data, long dim0, long dim1, long dim2, long dim3, long dim4, long dim5) where T : unmanaged
         {
-            ArrayExtensions.ValidateInputData(data, new long[] { dim0, dim1, dim2, dim3, dim4, dim5});
-            var output = new T[dim0, dim1, dim2, dim3, dim4, dim5];
+            var dims = new long[] { dim0, dim1, dim2, dim3, dim4, dim5 };
+            ArrayExtensions.ValidateInputData(data, dims);
+            var output = new T[dims[0], dims[1], dims[2], dims[3], dims[4], dims[5]];
 
             fixed (void* ptr = output)
             {
