a45d1e2c33fed980df2c75f9d5a8c163d2d0e69d
Apollo3zehn <git@m1.apollo3zehn.net>
2023-01-06 09:16:52 +0100

    Remove unnecessary test.

--- tests/HDF5.NET.Tests/AsyncTests.cs
+++ tests/HDF5.NET.Tests/AsyncTests.cs
@@ -8,34 +8,6 @@ namespace HDF5.NET.Tests.Reading
     public class AsyncTests
     {
         [Fact]
-        public async Task CanReadDataset_ChunkedBTree2()
-        {
-            // async version of test CanReadDataset_ChunkedBTree2
-            foreach (var withShuffle in new bool[] { false, true })
-            {
-                // Arrange
-                var version = H5F.libver_t.LATEST;
-                var filePath = TestUtils.PrepareTestFile(version, fileId => TestUtils.AddChunkedDataset_BTree2(fileId, withShuffle));
-
-                // Act
-                using var root = H5File.OpenCore(
-                    filePath,
-                    FileMode.Open, 
-                    FileAccess.Read, 
-                    FileShare.Read, 
-                    useAsync: true, 
-                    deleteOnClose: true);
-
-                var parent = root.Group("chunked");
-                var dataset = parent.Dataset("chunked_btree2");
-                var actual = await dataset.ReadAsync<int>();
-
-                // Assert
-                Assert.True(actual.SequenceEqual(TestData.MediumData));
-            }
-        }
-
-        [Fact]
         public async Task CanReadDatasetParallel()
         {
             // Arrange
