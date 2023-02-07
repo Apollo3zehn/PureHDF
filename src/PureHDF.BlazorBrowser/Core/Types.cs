69515e68794d3e3486b97504d3e2ac5b3b1d50fe
Apollo3zehn <git@m1.apollo3zehn.net>
2023-02-07 14:32:04 +0100

    Remove BlazorBrowser.

--- src/PureHDF.BlazorBrowser/Core/Types.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-
-namespace PureHDF.BlazorBrowser.Core
-{
-    public record FileContainer(
-        IBrowserFile BrowserFile,
-        H5File H5File);
-}
