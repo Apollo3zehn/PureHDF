805467cf9b723df160487e03cc221668d082e9c2
Apollo3zehn <git@m1.apollo3zehn.net>
2023-03-12 12:50:45 +0100

    Outsourcing of the HSDS client generator.

--- src/PureHDF.VOL.HsdsClientGenerator/GeneratorSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PureHDF.VOL.HsdsClientGenerator;
-
-public record GeneratorSettings(
-    string? Namespace,
-    string ClientName,
-    string ExceptionType,
-    Dictionary<string, string> PathToMethodNameMap);
