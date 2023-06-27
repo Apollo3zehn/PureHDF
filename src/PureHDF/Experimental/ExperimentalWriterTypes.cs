751f0324ac26dbfd4ea66263b9069cabfd93b8a1
Apollo3zehn <git@m1.apollo3zehn.net>
2023-06-27 11:56:50 +0200

    Prepare first writer test

---
+++ src/PureHDF/Experimental/ExperimentalWriterTypes.cs
@@ -0,0 +1,69 @@
+namespace PureHDF.Experimental;
+
+/// <summary>
+/// An H5 file.
+/// </summary>
+/// <param name="Attributes">The attributes that belong to the top-level group.</param>
+/// <param name="Objects">The objects that belong to the top-level group.</param>
+public record class H5File(
+    IReadOnlyList<H5AttributeBase> Attributes,
+    IReadOnlyList<H5Object> Objects)
+    : H5Group("/", Attributes, Objects);
+
+/// <summary>
+/// A group.
+/// </summary>
+/// <param name="Name">The group name.</param>
+/// <param name="Attributes">The attributes that belong to this group.</param>
+/// <param name="Objects">The objects that belong to this group.</param>
+public record class H5Group(
+    string Name,
+    IReadOnlyList<H5AttributeBase>? Attributes = default,
+    IReadOnlyList<H5Object>? Objects = default)
+    : H5AttributableObject(Name, Attributes);
+
+/// <summary>
+/// A dataset.
+/// </summary>
+/// <typeparam name="T">The type of data this dataset represents.</typeparam>
+/// <param name="Name">The dataset name.</param>
+/// <param name="Attributes">The attributes that belong to this dataset.</param>
+public record class H5Dataset<T>(
+    string Name,
+    IReadOnlyList<H5AttributeBase>? Attributes = default)
+    : H5AttributableObject(Name, Attributes);
+
+/// <summary>
+/// A base class for attributable objects.
+/// </summary>
+/// <param name="Name">The object name.</param>
+/// <param name="Attributes">The attributes that belong to this object.</param>
+public record class H5AttributableObject(
+    string Name,
+    IReadOnlyList<H5AttributeBase>? Attributes)
+    : H5Object(Name);
+
+/// <summary>
+/// A base class for objects.
+/// </summary>
+/// <param name="Name">The object name.</param>
+public record class H5Object(
+    string Name);
+
+/// <summary>
+/// A generic attribute.
+/// </summary>
+/// <typeparam name="T">The type of data this attribute represents.</typeparam>
+/// <param name="Name">The attribute name.</param>
+/// <param name="data">The attribute data.</param>
+public record class H5Attribute<T>(
+    string Name,
+    T[] data)
+    : H5AttributeBase(Name) where T : unmanaged;
+
+/// <summary>
+/// Non-generic base class for attributes.
+/// </summary>
+/// <param name="Name">The attribute name.</param>
+public abstract record class H5AttributeBase(
+    string Name);
---
+++ tests/PureHDF.Tests/WriterTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using PureHDF.Experimental;
+using Xunit;
+
+namespace PureHDF.Tests.Writing;
+
+public class ChunkCacheTests
+{
+    [Fact]
+    public void CanWrite()
+    {
+        // Arrange
+        var attribute_0 = new H5Attribute<Point>("attribute_0", new Point[] { new Point(x: 10, y: 20), new Point(x: 20, y: 30) });
+        var attribute_1 = new H5Attribute<uint>("attribute_1", new uint[] { 1, 2, 3 });
+        var attribute_g0_0 = new H5Attribute<double>("attribute_g0_0", new double[] { 2.0, 3.1, 4.2 });
+        var attribute_g0_d0_0 = new H5Attribute<int>("attribute_g0_d0_0", new int[] { -2, -3, -4 });
+
+        var dataset_g0_0 = new H5Dataset<ushort>(
+            Name: "dataset_g0_0",
+            Attributes: new List<H5AttributeBase>() { attribute_g0_d0_0 }
+        );
+
+        var dataset_0 = new H5Dataset<long>(
+            Name: "dataset_0",
+            Attributes: new List<H5AttributeBase>()
+        );
+
+        var group_0 = new H5Group(
+            Name: "group1", 
+            Attributes: new List<H5AttributeBase>() { attribute_g0_0 },
+            Objects: new List<H5Object>() { dataset_g0_0 });
+
+        var group_1 = new H5Group(
+            Name: "group1");
+
+        var expected = new Experimental.H5File(
+            Attributes: new List<H5AttributeBase>() { attribute_0, attribute_1 },
+            Objects: new List<H5Object>() { group_0, group_1, dataset_0 });
+
+        var filePath = "my-path";
+
+        // TODO: continue here
+
+        // Act
+        // expected.Save(filePath);
+        // var actual = H5File.OpenRead(filePath);
+
+        // Assert
+    }
+}
