2a618eec1cb0fea766ac3da2c796a79c25380dc8
Apollo3zehn <git@m1.apollo3zehn.net>
2024-03-25 21:28:54 +0100

    Fix #67

--- src/PureHDF/API.Writing/H5WriteOptions.cs
+++ src/PureHDF/API.Writing/H5WriteOptions.cs
@@ -6,7 +6,7 @@ namespace PureHDF;
 /// Provides options to be used with <see cref="H5File"/>.
 /// </summary>
 /// <param name="DefaultStringLength">A value that indicates how strings are handled during serialization. A nonzero positve value means that strings are treated as fixed-length strings, otherwise they are variable-length strings. The default value is 0.</param>
-/// <param name="GlobalHeapCollectionSize">A value that indicates the size of a global heap collection in bytes. The default value is 4096 bytes which is the minimum allowed size.</param>
+/// <param name="MinimumGlobalHeapCollectionSize">A value that indicates the minimum size of a global heap collection in bytes. The default value is 4096 bytes which is the absolute minimum allowed size.</param>
 /// <param name="GlobalHeapFlushThreshold">A value that indicates the threshold after which global heap collections will be flushed. The default value is 4096 * 1024 = 4 MB.</param>
 /// <param name="PreferCompactDatasetLayout">A value that indicates whether the writer tries to use the compact layout for datasets if the total data size is &lt; 64 kB and it should not be chunked.</param>
 /// <param name="IncludeStructFields">A value that indicates whether struct fields are handled during serialization. The default value is <see langword="true"/>.</param>
@@ -20,7 +20,7 @@ namespace PureHDF;
 /// <param name="Filters">A list of filters and their options to be applied to datasets that have no explicit filters assigned.</param>
 public record H5WriteOptions(
     int DefaultStringLength = default,
-    int GlobalHeapCollectionSize = 4096,
+    int MinimumGlobalHeapCollectionSize = 4096,
     long GlobalHeapFlushThreshold = 4096 * 1024,
     bool PreferCompactDatasetLayout = true,
     bool IncludeStructFields = true,
