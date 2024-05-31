72cf22f105de5488c86e90dbe2e106dcfe223fa4
Apollo3zehn <git@m1.apollo3zehn.net>
2024-05-31 11:27:50 +0200

    Add benchmark

--- benchmarks/PureHDF.Benchmarks/Program.cs
+++ benchmarks/PureHDF.Benchmarks/Program.cs
@@ -1,20 +1,5 @@
 using BenchmarkDotNet.Running;
 
-namespace Benchmark;
-
-public class Program
-{
-    static void Main(string[] args)
-    {
-        if (args.Any())
-        {
-            _ = args[0] switch
-            {
-                "inflate" => BenchmarkRunner.Run<Inflate>(),
-                "walk" => BenchmarkRunner.Run<Walk>(),
-                "shuffle" => BenchmarkRunner.Run<Shuffle>(),
-                _ => throw new Exception($"Unknown benchmark {args[0]}")
-            };
-        }
-    }
-}
+BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run(args);
